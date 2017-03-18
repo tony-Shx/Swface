4a3fd528941ab935ed5e693448c5934e5e1601a1
Tony-Shx <17839223557@163.com>
2017-03-18 19:08:25 +0800

    拍照可以保存到本地“

---
+++ app/src/main/java/com/henu/swface/DialogInputNameActivity.java
@@ -0,0 +1,52 @@
+package com.henu.swface;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class DialogInputNameActivity extends Activity implements View.OnClickListener {
+	private Button button_ok,button_cancel;
+	private EditText editText;
+	private byte[] data;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_dialog_input_name);
+		data = getIntent().getByteArrayExtra("data");
+		button_ok = (Button) findViewById(R.id.button_dialog_input_ok);
+		button_cancel = (Button) findViewById(R.id.button_dialog_input_cancel);
+		editText = (EditText) findViewById(R.id.editText_dialog_username);
+		button_ok.setOnClickListener(this);
+		button_cancel.setOnClickListener(this);
+
+	}
+
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()){
+			case R.id.button_dialog_input_ok:
+				String username = editText.getText().toString();
+				Intent intent = new Intent();
+				intent.putExtra("username",username);
+				setResult(1,intent);
+				finish();
+				break;
+			case R.id.button_dialog_input_cancel:
+				setResult(0);
+				finish();
+				break;
+			default:
+				break;
+		}
+	}
+}
--- app/src/main/java/com/henu/swface/VideoDemo.java
+++ app/src/main/java/com/henu/swface/VideoDemo.java
@@ -3,6 +3,7 @@ package com.henu.swface;
 import android.Manifest.permission;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Canvas;
 import android.graphics.ImageFormat;
@@ -14,6 +15,7 @@ import android.hardware.Camera.CameraInfo;
 import android.hardware.Camera.Parameters;
 import android.hardware.Camera.PreviewCallback;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Process;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -24,6 +26,7 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
+import android.widget.Button;
 import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.RelativeLayout;
@@ -34,13 +37,15 @@ import com.henu.swface.util.FaceRect;
 import com.henu.swface.util.FaceUtil;
 import com.henu.swface.util.ParseResult;
 import com.iflytek.cloud.FaceDetector;
+import com.iflytek.cloud.SpeechUtility;
 import com.iflytek.cloud.util.Accelerometer;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
  * 离线视频流检测
- *
  */
 public class VideoDemo extends Activity {
 	private final static String TAG = VideoDemo.class.getSimpleName();
@@ -64,49 +69,49 @@ public class VideoDemo extends Activity {
 	private Toast mToast;
 	private long mLastClickTime;
 	private int isAlign = 0;
-	
+	private Button button_take_photos;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		SpeechUtility.createUtility(this, "appid=58c8e767");
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_video_demo);
-	
+		setContentView(R.layout.activity_video);
 		initUI();
-		
 		nv21 = new byte[PREVIEW_WIDTH * PREVIEW_HEIGHT * 2];
 		buffer = new byte[PREVIEW_WIDTH * PREVIEW_HEIGHT * 2];
 		mAcc = new Accelerometer(VideoDemo.this);
 		mFaceDetector = FaceDetector.createDetector(VideoDemo.this, null);
 	}
-	
-	
+
+
 	private Callback mPreviewCallback = new Callback() {
-		
+
 		@Override
 		public void surfaceDestroyed(SurfaceHolder holder) {
 			closeCamera();
 		}
-		
+
 		@Override
 		public void surfaceCreated(SurfaceHolder holder) {
 			openCamera();
 		}
-		
+
 		@Override
 		public void surfaceChanged(SurfaceHolder holder, int format, int width,
 								   int height) {
-			mScaleMatrix.setScale(width/(float)PREVIEW_HEIGHT, height/(float)PREVIEW_WIDTH);
+			mScaleMatrix.setScale(width / (float) PREVIEW_HEIGHT, height / (float) PREVIEW_WIDTH);
 		}
 	};
-	
+
 	private void setSurfaceSize() {
 		DisplayMetrics metrics = new DisplayMetrics();
 		getWindowManager().getDefaultDisplay().getMetrics(metrics);
-		
+
 		int width = metrics.widthPixels;
-		int height = (int) (width * PREVIEW_WIDTH / (float)PREVIEW_HEIGHT);
+		int height = (int) (width * PREVIEW_WIDTH / (float) PREVIEW_HEIGHT);
 		LayoutParams params = new LayoutParams(width, height);
 		params.addRule(RelativeLayout.ALIGN_PARENT_TOP);
-		
+
 		mPreviewSurface.setLayoutParams(params);
 		mFaceSurface.setLayoutParams(params);
 	}
@@ -114,17 +119,17 @@ public class VideoDemo extends Activity {
 	@SuppressLint("ShowToast")
 	@SuppressWarnings("deprecation")
 	private void initUI() {
+		button_take_photos = (Button) findViewById(R.id.button_take_photos);
 		mPreviewSurface = (SurfaceView) findViewById(R.id.sfv_preview);
 		mFaceSurface = (SurfaceView) findViewById(R.id.sfv_face);
-		
 		mPreviewSurface.getHolder().addCallback(mPreviewCallback);
 		mPreviewSurface.getHolder().setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 		mFaceSurface.setZOrderOnTop(true);
 		mFaceSurface.getHolder().setFormat(PixelFormat.TRANSLUCENT);
-		
+
 		// 点击SurfaceView，切换摄相头
 		mFaceSurface.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// 只有一个摄相头，不支持切换
@@ -141,68 +146,122 @@ public class VideoDemo extends Activity {
 				openCamera();
 			}
 		});
-		
+
 		// 长按SurfaceView 500ms后松开，摄相头聚集
 		mFaceSurface.setOnTouchListener(new OnTouchListener() {
-			
+
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
 				switch (event.getAction()) {
-				case MotionEvent.ACTION_DOWN:
-					mLastClickTime = System.currentTimeMillis();
-					break;
-				case MotionEvent.ACTION_UP:
-					if (System.currentTimeMillis() - mLastClickTime > 500) {
-						mCamera.autoFocus(null);
-						return true;
-					}
-					break;
-					
-				default:
-					break;
+					case MotionEvent.ACTION_DOWN:
+						mLastClickTime = System.currentTimeMillis();
+						break;
+					case MotionEvent.ACTION_UP:
+						if (System.currentTimeMillis() - mLastClickTime > 500) {
+							mCamera.autoFocus(null);
+							return true;
+						}
+						break;
+
+					default:
+						break;
 				}
 				return false;
 			}
 		});
-		
+
 		RadioGroup alignGruop = (RadioGroup) findViewById(R.id.align_mode);
 		alignGruop.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-			
+
 			@Override
 			public void onCheckedChanged(RadioGroup arg0, int arg1) {
 				switch (arg1) {
-				case R.id.detect:
-					isAlign = 0;
-					break;
-				case R.id.align:
-					isAlign = 1;
-					break;
-				default:
-					break;
+					case R.id.detect:
+						isAlign = 0;
+						break;
+					case R.id.align:
+						isAlign = 1;
+						break;
+					default:
+						break;
 				}
 			}
 		});
-		
+
 		setSurfaceSize();
 		mToast = Toast.makeText(VideoDemo.this, "", Toast.LENGTH_SHORT);
+
+		button_take_photos.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Camera.PictureCallback jpeg = new Camera.PictureCallback() {
+					@Override
+					public void onPictureTaken(byte[] data, Camera camera) {
+						String filename = "waitForRename.jpg";
+						FileOutputStream fos = null;
+						try {
+							fos = new FileOutputStream(
+									new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) +
+											"/" + filename));
+							fos.write(data);
+							fos.flush();
+							fos.close();
+							System.out.println("图片保存成功");
+							Intent intent = new Intent(getApplicationContext(), DialogInputNameActivity.class);
+							//intent.putExtra("data",data);
+							startActivityForResult(intent, 0);
+
+						} catch (Exception e) {
+							System.out.println("图片保存异常" + e.getMessage());
+							e.printStackTrace();
+						}
+
+					}
+				};
+				mCamera.takePicture(null, null, jpeg);
+			}
+		});
 	}
-	
+
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (resultCode == 1) {
+			File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) +
+					"/waitForRename.jpg");
+			if (file.exists()) {
+				String username = data.getStringExtra("username");
+				File newfile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+						+ "/" + username + ".jpg");
+				file.renameTo(newfile);
+				System.out.println("图片重命名成功");
+			} else {
+				Toast.makeText(this, "拍照保存失败，错误代码-1", Toast.LENGTH_LONG);
+			}
+			mCamera.startPreview();//保存之后返回预览界面
+			finish();
+		} else {
+			mCamera.startPreview();//保存之后返回预览界面
+		}
+	}
+
 	private void openCamera() {
 		if (null != mCamera) {
 			return;
 		}
-		
+
 		if (!checkCameraPermission()) {
 			showTip("摄像头权限未打开，请打开后再试");
 			mStopTrack = true;
 			return;
 		}
-		
+
 		// 只有一个摄相头，打开后置
 		if (Camera.getNumberOfCameras() == 1) {
 			mCameraId = CameraInfo.CAMERA_FACING_BACK;
 		}
-		
+
 		try {
 			mCamera = Camera.open(mCameraId);
 			if (CameraInfo.CAMERA_FACING_FRONT == mCameraId) {
@@ -215,39 +274,39 @@ public class VideoDemo extends Activity {
 			closeCamera();
 			return;
 		}
-		
+
 		Parameters params = mCamera.getParameters();
 		params.setPreviewFormat(ImageFormat.NV21);
 		params.setPreviewSize(PREVIEW_WIDTH, PREVIEW_HEIGHT);
 		mCamera.setParameters(params);
-		
+
 		// 设置显示的偏转角度，大部分机器是顺时针90度，某些机器需要按情况设置
 		mCamera.setDisplayOrientation(90);
 		mCamera.setPreviewCallback(new PreviewCallback() {
-			
+
 			@Override
 			public void onPreviewFrame(byte[] data, Camera camera) {
 				System.arraycopy(data, 0, nv21, 0, data.length);
 			}
 		});
-		
+
 		try {
 			mCamera.setPreviewDisplay(mPreviewSurface.getHolder());
 			mCamera.startPreview();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		if(mFaceDetector == null) {
+
+		if (mFaceDetector == null) {
 			/**
 			 * 离线视频流检测功能需要单独下载支持离线人脸的SDK
 			 * 请开发者前往语音云官网下载对应SDK
 			 */
 			// 创建单例失败，与 21001 错误为同样原因，参考 http://bbs.xfyun.cn/forum.php?mod=viewthread&tid=9688
-			showTip( "创建对象失败，请确认 libmsc.so 放置正确，\n 且有调用 createUtility 进行初始化" );
+			showTip("创建对象失败，请确认 libmsc.so 放置正确，\n 且有调用 createUtility 进行初始化");
 		}
 	}
-	
+
 	private void closeCamera() {
 		if (null != mCamera) {
 			mCamera.setPreviewCallback(null);
@@ -256,38 +315,38 @@ public class VideoDemo extends Activity {
 			mCamera = null;
 		}
 	}
-	
+
 	private boolean checkCameraPermission() {
 		int status = checkPermission(permission.CAMERA, Process.myPid(), Process.myUid());
 		if (PackageManager.PERMISSION_GRANTED == status) {
 			return true;
 		}
-		
+
 		return false;
 	}
-	
+
 	@Override
 	protected void onResume() {
 		super.onResume();
-		
+
 		if (null != mAcc) {
 			mAcc.start();
 		}
-		
+
 		mStopTrack = false;
 		new Thread(new Runnable() {
-			
+
 			@Override
 			public void run() {
 				while (!mStopTrack) {
 					if (null == nv21) {
 						continue;
 					}
-					
+
 					synchronized (nv21) {
 						System.arraycopy(nv21, 0, buffer, 0, nv21.length);
 					}
-					
+
 					// 获取手机朝向，返回值0,1,2,3分别表示0,90,180和270度
 					int direction = Accelerometer.getDirection();
 					boolean frontCamera = (CameraInfo.CAMERA_FACING_FRONT == mCameraId);
@@ -295,58 +354,58 @@ public class VideoDemo extends Activity {
 					// 转换公式：a' = (360 - a)%360，a为人眼视角下的朝向（单位：角度）
 					if (frontCamera) {
 						// SDK中使用0,1,2,3,4分别表示0,90,180,270和360度
-						direction = (4 - direction)%4;
+						direction = (4 - direction) % 4;
 					}
 
-					if(mFaceDetector == null) {
+					if (mFaceDetector == null) {
 						/**
 						 * 离线视频流检测功能需要单独下载支持离线人脸的SDK
 						 * 请开发者前往语音云官网下载对应SDK
 						 */
 						// 创建单例失败，与 21001 错误为同样原因，参考 http://bbs.xfyun.cn/forum.php?mod=viewthread&tid=9688
-						showTip( "创建对象失败，请确认 libmsc.so 放置正确，\n 且有调用 createUtility 进行初始化" );
+						showTip("创建对象失败，请确认 libmsc.so 放置正确，\n 且有调用 createUtility 进行初始化");
 						break;
 					}
-					
+
 					String result = mFaceDetector.trackNV21(buffer, PREVIEW_WIDTH, PREVIEW_HEIGHT, isAlign, direction);
-					Log.d(TAG, "result:"+result);
-					
+					Log.d(TAG, "result:" + result);
+
 					FaceRect[] faces = ParseResult.parseResult(result);
-					
+
 					Canvas canvas = mFaceSurface.getHolder().lockCanvas();
 					if (null == canvas) {
 						continue;
 					}
-					
+
 					canvas.drawColor(0, PorterDuff.Mode.CLEAR);
 					canvas.setMatrix(mScaleMatrix);
 
-					if( faces == null || faces.length <=0 ) {
+					if (faces == null || faces.length <= 0) {
 						mFaceSurface.getHolder().unlockCanvasAndPost(canvas);
 						continue;
 					}
-					
+
 					if (null != faces && frontCamera == (CameraInfo.CAMERA_FACING_FRONT == mCameraId)) {
-						for (FaceRect face: faces) {
+						for (FaceRect face : faces) {
 							face.bound = FaceUtil.RotateDeg90(face.bound, PREVIEW_WIDTH, PREVIEW_HEIGHT);
 							if (face.point != null) {
 								for (int i = 0; i < face.point.length; i++) {
 									face.point[i] = FaceUtil.RotateDeg90(face.point[i], PREVIEW_WIDTH, PREVIEW_HEIGHT);
 								}
 							}
-							FaceUtil.drawFaceRect(canvas, face, PREVIEW_WIDTH, PREVIEW_HEIGHT, 
+							FaceUtil.drawFaceRect(canvas, face, PREVIEW_WIDTH, PREVIEW_HEIGHT,
 									frontCamera, false);
 						}
 					} else {
 						Log.d(TAG, "faces:0");
 					}
-					
+
 					mFaceSurface.getHolder().unlockCanvasAndPost(canvas);
 				}
 			}
 		}).start();
 	}
-	
+
 	@Override
 	protected void onPause() {
 		super.onPause();
@@ -356,16 +415,16 @@ public class VideoDemo extends Activity {
 		}
 		mStopTrack = true;
 	}
-	
+
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
-		if( null != mFaceDetector ){
+		if (null != mFaceDetector) {
 			// 销毁对象
 			mFaceDetector.destroy();
 		}
 	}
-	
+
 	private void showTip(final String str) {
 		mToast.setText(str);
 		mToast.show();

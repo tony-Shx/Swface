7ac35d1ab2c752b4e20700429a7cc7d0348e0a2d
Tony-Shx <17839223557@163.com>
2017-03-18 14:30:50 +0800

    注册新用户调用摄像头完成“

--- app/src/main/java/com/henu/swface/MainActivity.java
+++ app/src/main/java/com/henu/swface/MainActivity.java
@@ -1,15 +1,43 @@
 package com.henu.swface;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+	private Button button_register,button_sign_in;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		initUI();
 
 
+	}
+
+	private void initUI() {
+		button_register = (Button) this.findViewById(R.id.button_register);
+		button_sign_in = (Button) this.findViewById(R.id.button_sign_in);
+		button_register.setOnClickListener(this);
+		button_sign_in.setOnClickListener(this);
+
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()){
+			case R.id.button_register:
+				Intent intent = new Intent(this,VideoDemo.class);
+				startActivity(intent);
+				break;
+			case R.id.button_sign_in:
+
+				break;
+			default:
+				break;
+		}
 	}
 }
